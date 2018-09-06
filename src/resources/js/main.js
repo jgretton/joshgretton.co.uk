db5938af4177ff81de8e3a76a1a015ea8036e5be
Joshua Gretton <jb.gretton@googlemail.com>
2018-09-06 20:44:58 +0100

    Header + menu functionality

---
+++ src/resources/js/main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('.nav');
+const navLink = document.querySelectorAll('.nav-link');
+const menu = document.querySelector('.menu-bars');
+const body = document.querySelector('body');
+
+let showMenu = false;
+
+menu.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        nav.classList.add('open');
+        menu.classList.add('close')
+        body.classList.add('darken')
+
+        showMenu = true;
+    }
+
+    else{
+        nav.classList.remove('open');
+        menu.classList.remove('close');
+        body.classList.remove('darken');
+
+        showMenu = false;
+    }
+
+}
